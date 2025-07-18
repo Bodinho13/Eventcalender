195ba499ebfcb9928b7133ddc00802eefd5113bf
Marcus Bode <bodemarcus13@gmail.com>
2025-07-18 11:47:19 +0200

    Replaced the gimmick obj of event with an array

--- src/components/EwEventForm.tsx
+++ src/components/EwEventForm.tsx
@@ -8,12 +8,13 @@ const EwEventForm = () => {
         eventName: "",
         host: "",
         date: new Date(),
-        startTime: 0,
-        endTime: 0,
+        startTime: "",
+        endTime: "",
         eventCategory: "",
         entryFee: 0,
         additionalInfo: "",
-        gimmicks: {
+        extras: [""]
+        /*gimmicks: {
             comedyshow: false,
             dancefloor: false,
             darts: false,
@@ -25,7 +26,7 @@ const EwEventForm = () => {
             pizza: false,
             tea: false,
             waffles: false
-        }
+        }*/
     });
 
     const changeEventData = (e: React.ChangeEvent) => {
@@ -35,6 +36,20 @@ const EwEventForm = () => {
         
         setEvent(values => ({...values, [name]: value}))
         console.log(event, value);
+    }
+
+    const changeExtras = (e: React.ChangeEvent) => {
+        const target = e.target as HTMLFormElement;
+        const name: string = target.name;
+
+        let extras = event.extras;
+        if (extras.indexOf(name) > -1) {
+            extras.splice(extras.indexOf(name, 0), 1);
+        } else {
+            extras.push(name);
+        }
+        setEvent(values => ({...values, extras: extras}))
+        console.log(event);
     }
 
     const submitEvent = (formData: any) => {
@@ -71,26 +86,26 @@ const EwEventForm = () => {
                 
                 <label className="ew-label">
                     <span className="ew-label-text">Extras:</span>
-                    <input className="ew-input-check" type="checkbox" name="comedyshow"/> Comedy-Show
-                    <input className="ew-input-check" type="checkbox" name="darts"/> Darts
-                    <input className="ew-input-check" type="checkbox" name="dj"/> DJ
+                    <input className="ew-input-check" type="checkbox" name="comedyshow" onChange={e => changeExtras(e)}/> Comedy-Show
+                    <input className="ew-input-check" type="checkbox" name="darts" onChange={e => changeExtras(e)}/> Darts
+                    <input className="ew-input-check" type="checkbox" name="dj" onChange={e => changeExtras(e)}/> DJ
                 </label>
                 <label className="ew-label">
                     <span className="ew-label-text"></span>
-                    <input className="ew-input-check" type="checkbox" name="drivingService"/> Fahrservice
-                    <input className="ew-input-check" type="checkbox" name="grill"/> Grill
-                    <input className="ew-input-check" type="checkbox" name="kicker"/> Kicker
+                    <input className="ew-input-check" type="checkbox" name="drivingService" onChange={e => changeExtras(e)}/> Fahrservice
+                    <input className="ew-input-check" type="checkbox" name="grill" onChange={e => changeExtras(e)}/> Grill
+                    <input className="ew-input-check" type="checkbox" name="kicker" onChange={e => changeExtras(e)}/> Kicker
                 </label>
                 <label className="ew-label">
                     <span className="ew-label-text"></span>
-                    <input className="ew-input-check" type="checkbox" name="livemusic"/> Live-Musik
-                    <input className="ew-input-check" type="checkbox" name="pizza"/> Pizza
-                    <input className="ew-input-check" type="checkbox" name="dancefloor"/> Tanzfläche
+                    <input className="ew-input-check" type="checkbox" name="livemusic" onChange={e => changeExtras(e)}/> Live-Musik
+                    <input className="ew-input-check" type="checkbox" name="pizza" onChange={e => changeExtras(e)}/> Pizza
+                    <input className="ew-input-check" type="checkbox" name="dancefloor" onChange={e => changeExtras(e)}/> Tanzfläche
                 </label>
                 <label className="ew-label">
                     <span className="ew-label-text"></span>
-                    <input className="ew-input-check" type="checkbox" name="tea"/> Tee-Stand
-                    <input className="ew-input-check" type="checkbox" name="waffles"/> Waffel-Stand
+                    <input className="ew-input-check" type="checkbox" name="tea" onChange={e => changeExtras(e)}/> Tee-Stand
+                    <input className="ew-input-check" type="checkbox" name="waffles" onChange={e => changeExtras(e)}/> Waffel-Stand
                 </label>
             </form>
         </div>
